107c07fd6d2f79a5a172a4740713ed7fd00e74ac
Radeonxz <xuanxxzhao@gmail.com>
2020-03-24 23:09:41 -0400

    chore: add string

--- TypeScript/y1/app.ts
+++ TypeScript/y1/app.ts
@@ -1,15 +1,22 @@
-const add = (n1: number, n2: number, showResult: boolean) => {
+const add = (
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  resultStr: string
+) => {
+  const result = n1 + n2;
   if (showResult) {
-    console.log(n1 + n2);
+    console.log(resultStr + result);
   } else {
-    return n1 + n2;
+    return result;
   }
 };
 
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultString = "Result is: ";
 
-const result = add(number1, number2, printResult);
+const result = add(number1, number2, printResult, resultString);
 
 console.log("result is ", result);
